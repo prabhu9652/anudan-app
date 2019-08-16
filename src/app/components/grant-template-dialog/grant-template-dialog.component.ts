211ae0edb0a2f33c1fa8736dab13d6a50011b311
Ranjit Victor <ranjitvictor@Ranjits-MacBook-Pro.local>
2019-08-16 15:27:24 +0530

    Added grant template selection

---
+++ src/app/components/grant-template-dialog/grant-template-dialog.component.ts
@@ -0,0 +1,29 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {GrantTemplate} from '../../model/dahsboard';
+
+import {MAT_DIALOG_DATA, MatDialogModule, MatDialogRef, MatButtonModule} from '@angular/material';
+
+@Component({
+  selector: 'app-grant-template-dialog',
+  templateUrl: './grant-template-dialog.component.html',
+  styleUrls: ['./grant-template-dialog.component.scss']
+})
+export class GrantTemplateDialogComponent implements OnInit {
+
+
+  constructor(public dialogRef: MatDialogRef<GrantTemplateDialogComponent>
+      , @Inject(MAT_DIALOG_DATA) public message: string) {
+    this.dialogRef.disableClose = true;
+  }
+
+  ngOnInit() {
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close(false);
+  }
+
+  onYesClick(): void {
+    this.dialogRef.close(true);
+  }
+}
