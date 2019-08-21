ba146f58851bb608c03022b6bdee09ee034d51b6
Ranjit Victor <ranjitvictor@Ranjits-MacBook-Pro.local>
2019-08-21 21:44:33 +0530

    Fixed issue in order SDK

--- src/app/orderby.ts
+++ src/app/orderby.ts
@@ -1,5 +1,5 @@
 //The pipe class implements the PipeTransform interface's transform method that accepts an input value and an optional array of parameters and returns the transformed value.
-import { Pipe,PipeTransform } from "angular2/core";
+import { Pipe,PipeTransform } from "'@angular/core";
 //We tell Angular that this is a pipe by applying the @Pipe decorator which we import from the core Angular library.
 @Pipe({
   //The @Pipe decorator takes an object with a name property whose value is the pipe name that we'll use within a template expression. It must be a valid JavaScript identifier. Our pipe's name is orderby.
