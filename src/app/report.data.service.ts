d4eefe05f3b3ca8d360fd62872f32ab91ff3bc60
Ranjit Victor <ranjit@socialalpha.org>
2020-07-09 20:29:47 +0530

    Delete report functionality enabled

--- src/app/report.data.service.ts
+++ src/app/report.data.service.ts
@@ -1,20 +1,62 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
-import { Report} from './model/report'
+import { Report } from './model/report'
+import { UserService } from './user.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ReportDataService {
 
+  url: string = "/api/user/%USERID%/report";
+
   private messageSource = new BehaviorSubject<Report[]>(null);
   currentMessage = this.messageSource.asObservable();
 
-  constructor() { }
+  constructor(private httpClient: HttpClient, public userService: UserService) { }
 
   changeMessage(message: Report[]) {
     this.messageSource.next(message)
+  }
+
+  deleteReport(report: Report): Promise<void> {
+    if (report !== undefined && report !== null) {
+      return this.httpClient
+        .delete(
+          this.getUrl() + "/" + report.id,
+          this.getHeader()
+        )
+        .toPromise()
+        .then<void>(() => {
+          return Promise.resolve();
+        })
+        .catch((err) => {
+          return Promise.reject(
+            "Unable to delete the disbursement"
+          );
+        });
+    } else {
+      return Promise.resolve(null);
+    }
+  }
+
+  private getHeader() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json",
+        "X-TENANT-CODE": localStorage.getItem("X-TENANT-CODE"),
+        Authorization: localStorage.getItem("AUTH_TOKEN"),
+      }),
+    };
+    return httpOptions;
+  }
+  private getUrl(): string {
+    const user = this.userService.getUser();
+    if (user !== undefined) {
+      return this.url.replace("%USERID%", user.id);
+    }
   }
 
 }
