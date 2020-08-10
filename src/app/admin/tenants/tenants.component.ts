4c65322b946b51c55dccfb6d21a36806900dd673
Ranjit Victor <ranjit@socialalpha.org>
2020-08-10 08:45:20 +0530

    Handle new tenant creation

--- src/app/admin/tenants/tenants.component.ts
+++ src/app/admin/tenants/tenants.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnInit, ViewChild, ElementRef} from '@angular/core';
-import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
-import {AppComponent} from '../../app.component';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { AppComponent } from '../../app.component';
 
 @Component({
   selector: 'app-tenants',
@@ -9,57 +9,57 @@ import {AppComponent} from '../../app.component';
 })
 export class TenantsComponent implements OnInit {
 
-    tenant: string;
+  tenant: string;
 
-    @ViewChild("tenantName") tenantNameElem: ElementRef;
-    @ViewChild("tenantAdmin") tenantAdminElem: ElementRef;
-    @ViewChild("tenantSlug") tenantSlugElem: ElementRef;
-    @ViewChild("tenantLogo") tenantLogoElem: ElementRef;
+  @ViewChild("tenantName") tenantNameElem: ElementRef;
+  @ViewChild("tenantAdmin") tenantAdminElem: ElementRef;
+  @ViewChild("tenantSlug") tenantSlugElem: ElementRef;
+  @ViewChild("tenantLogo") tenantLogoElem: ElementRef;
 
   constructor(
     private http: HttpClient,
     private appComp: AppComponent
-  ) {}
+  ) { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
-    createTenant(){
-        const tenantName = this.tenantNameElem.nativeElement.value;
-        const tenantAdmin = this.tenantAdminElem.nativeElement.value;
-        const tenantSlug = this.tenantSlugElem.nativeElement.value;
-        const tenantLogo = this.tenantLogoElem.nativeElement;
+  createTenant() {
+    const tenantName = this.tenantNameElem.nativeElement.value;
+    const tenantAdmin = this.tenantAdminElem.nativeElement.value;
+    const tenantSlug = this.tenantSlugElem.nativeElement.value;
+    const tenantLogo = this.tenantLogoElem.nativeElement;
 
-        let formData = new FormData();
-        for( let i=0; i< tenantLogo.files.length; i++){
-            formData.append('file', tenantLogo.files.item(i));
-        }
-
-        const endpoint = '/api/granter' + '/user/'+this.appComp.loggedInUser.id  + '/onboard/'+ tenantName + '/slug/'+ tenantSlug + '/granterUser/' + tenantAdmin;
-
-        const httpOptions = {
-            headers: new HttpHeaders({
-              'X-TENANT-CODE': localStorage.getItem('X-TENANT-CODE'),
-              'Authorization': localStorage.getItem('AUTH_TOKEN')
-            })
-            };
-
-            this.http.post(endpoint,formData, httpOptions).subscribe((data) => {
-                console.log(data);
-            });
+    let formData = new FormData();
+    for (let i = 0; i < tenantLogo.files.length; i++) {
+      formData.append('file', tenantLogo.files.item(i));
     }
 
-    encryptPassword(){
+    const endpoint = '/api/granter' + '/user/' + this.appComp.loggedInUser.id + '/onboard/' + tenantName + '/slug/' + tenantSlug + '/granterUser/' + tenantAdmin;
 
-        const httpOptions = {
-            headers: new HttpHeaders({
-              'X-TENANT-CODE': localStorage.getItem('X-TENANT-CODE'),
-              'Authorization': localStorage.getItem('AUTH_TOKEN')
-            })
-        };
-        let url = '/api/admin/'+this.appComp.loggedInUser.id+'/encryptpasswords/'+this.tenant;
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'X-TENANT-CODE': localStorage.getItem('X-TENANT-CODE'),
+        'Authorization': localStorage.getItem('AUTH_TOKEN')
+      })
+    };
 
-        this.http.post(url,{},httpOptions).subscribe(() => {
-            alert('Passwords Encrypted');
-        });
-    }
+    this.http.post(endpoint, formData, httpOptions).subscribe((data) => {
+      alert("Tenant created.")
+    });
+  }
+
+  encryptPassword() {
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'X-TENANT-CODE': localStorage.getItem('X-TENANT-CODE'),
+        'Authorization': localStorage.getItem('AUTH_TOKEN')
+      })
+    };
+    let url = '/api/admin/' + this.appComp.loggedInUser.id + '/encryptpasswords/' + this.tenant;
+
+    this.http.post(url, {}, httpOptions).subscribe(() => {
+      alert('Passwords Encrypted');
+    });
+  }
 }
