e9b109af406a57610edcc234fccf70d0794d6c0c
Ranjit Victor <ranjit@socialalpha.org>
2020-08-19 17:10:25 +0530

    Hotfix 1

--- src/app/components/granthistory/granthistory.component.ts
+++ src/app/components/granthistory/granthistory.component.ts
@@ -1,9 +1,9 @@
-import {Component, Inject, OnInit, ViewChild, ElementRef, Renderer2, HostListener} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialogModule, MatDialogRef, MatButtonModule} from '@angular/material';
-import {Grant, GrantHistory} from '../../model/dahsboard';
-import {ReportHistory} from '../../model/report';
-import {WorkflowTransition} from '../../model/workflow-transition';
-import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
+import { Component, Inject, OnInit, ViewChild, ElementRef, Renderer2, HostListener } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef, MatButtonModule } from '@angular/material';
+import { Grant, GrantHistory } from '../../model/dahsboard';
+import { ReportHistory } from '../../model/report';
+import { WorkflowTransition } from '../../model/workflow-transition';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { DisbursementHistory } from 'app/model/disbursement';
 
 declare var $: any;
@@ -17,61 +17,60 @@ declare var jsPlumb: any;
 })
 export class GranthistoryComponent implements OnInit {
 
-    grantHistory: GrantHistory[]=[];
-    reportHistory: ReportHistory[]=[];
-    disbursementHistory: DisbursementHistory[]=[];
+  grantHistory: GrantHistory[] = [];
+  reportHistory: ReportHistory[] = [];
+  disbursementHistory: DisbursementHistory[] = [];
 
   constructor(
     public dialogRef: MatDialogRef<GranthistoryComponent>
-     , @Inject(MAT_DIALOG_DATA) public data: any
-     , private http: HttpClient
-     , private renderer: Renderer2
-     , @Inject(ElementRef)er: ElementRef
-     ) {
+    , @Inject(MAT_DIALOG_DATA) public data: any
+    , private http: HttpClient
+    , private renderer: Renderer2
+    , @Inject(ElementRef) er: ElementRef
+  ) {
     this.dialogRef.disableClose = false;
   }
 
   ngOnInit() {
-  const httpOptions = {
-              headers: new HttpHeaders({
-                  'Content-Type': 'application/json',
-                  'X-TENANT-CODE': localStorage.getItem('X-TENANT-CODE'),
-                  'Authorization': localStorage.getItem('AUTH_TOKEN')
-              })
-          };
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'X-TENANT-CODE': localStorage.getItem('X-TENANT-CODE'),
+        'Authorization': localStorage.getItem('AUTH_TOKEN')
+      })
+    };
 
-        if(this.data.type==='grant'){
-            const url = '/api/user/' + JSON.parse(localStorage.getItem('USER')).id+ '/grant/'+this.data.data.id+'/history/';
+    if (this.data.type === 'grant') {
+      const url = '/api/user/' + JSON.parse(localStorage.getItem('USER')).id + '/grant/' + this.data.data.id + '/history/';
 
-            this.http.get<GrantHistory[]>(url, httpOptions).subscribe((history: GrantHistory[]) => {
-                this.grantHistory = history;
-            });
-        }else if(this.data.type==='report'){
-            const url = '/api/user/' + JSON.parse(localStorage.getItem('USER')).id+ '/report/'+this.data.data.id+'/history/';
+      this.http.get<GrantHistory[]>(url, httpOptions).subscribe((history: GrantHistory[]) => {
+        this.grantHistory = history;
+      });
+    } else if (this.data.type === 'report') {
+      const url = '/api/user/' + JSON.parse(localStorage.getItem('USER')).id + '/report/' + this.data.data.id + '/history/';
 
-            this.http.get<ReportHistory[]>(url, httpOptions).subscribe((history: ReportHistory[]) => {
-                this.reportHistory = history;
-            });
-        }else if(this.data.type==='disbursement'){
-          const url = '/api/user/' + JSON.parse(localStorage.getItem('USER')).id+ '/disbursements/'+this.data.data.id+'/history/';
+      this.http.get<ReportHistory[]>(url, httpOptions).subscribe((history: ReportHistory[]) => {
+        this.reportHistory = history;
+      });
+    } else if (this.data.type === 'disbursement') {
+      const url = '/api/user/' + JSON.parse(localStorage.getItem('USER')).id + '/disbursements/' + this.data.data.id + '/history/';
 
-          this.http.get<DisbursementHistory[]>(url, httpOptions).subscribe((history: DisbursementHistory[]) => {
-              this.disbursementHistory = history;
-          });
-      }
+      this.http.get<DisbursementHistory[]>(url, httpOptions).subscribe((history: DisbursementHistory[]) => {
+        this.disbursementHistory = history;
+      });
+    }
 
 
   }
 
- onNoClick(): void {
+  onNoClick(): void {
     this.dialogRef.close(false);
   }
 
   onYesClick(): void {
     this.dialogRef.close(false);
   }
-  ngAfterViewInit(){
-    //this.showFlow(this.transitions);
+  ngAfterViewInit() {
   }
 
 }
